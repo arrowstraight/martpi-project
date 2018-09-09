5326f2bb1e0ff17367becc400325e68d3f4b900a
arrowstraight <37030740+arrowstraight@users.noreply.github.com>
2018-09-09 15:21:17 +0100

    added new files

---
+++ martinarrays.js
@@ -0,0 +1,76 @@
+const getNthElement = (index, array) => {
+
+    const arrayToCSVString = array => {
+
+    };
+
+    const csvStringToArray = string => {
+
+    };
+
+    const addToArray = (element, array) => {
+
+    };
+
+    const addToArray2 = (element, array) => {
+
+    };
+
+    const removeNthElement = (index, array) => {
+
+    };
+
+    const numbersToStrings = numbers => {
+
+    };
+
+    const uppercaseWordsInArray = strings => {
+
+    };
+
+    const reverseWordsInArray = strings => {
+
+    };
+
+    const onlyEven = numbers => {
+
+    };
+
+    const removeNthElement2 = (index, array) => {
+
+    };
+
+    const elementsStartingWithAVowel = strings => {
+
+    };
+
+    const removeSpaces = string => {
+
+    };
+
+    const sumNumbers = numbers => {
+
+    };
+
+    const sortByLastLetter = strings => {
+
+    };
+
+    module.exports = {
+        getNthElement,
+        arrayToCSVString,
+        csvStringToArray,
+        addToArray,
+        addToArray2,
+        removeNthElement,
+        numbersToStrings,
+        uppercaseWordsInArray,
+        reverseWordsInArray,
+        onlyEven,
+        removeNthElement2,
+        elementsStartingWithAVowel,
+        removeSpaces,
+        sumNumbers,
+        sortByLastLetter
+    }
+};
